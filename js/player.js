2b9f3c2d25d7efe2b6b30c2b6f6cd4840f254285
maxcepenco <cep211993@gmail.com>
2024-04-25 13:53:00 +0300

    first commit

--- js/player.js
+++ js/player.js
@@ -1,30 +1,31 @@
 // ---------------DAtA-------------
 let playlist = {
 	title: 'Hip-hop Hits',
-	coverImage: './Playlist1.jpeg',
+	coverImage: '/img/Playlist1.jpeg',
 	info: {
 		totalTracksCount: 4,
 		totalTracksDurationInSecond: 733,
 	},
 	tracks: [
 		{
-			coverImageUrl: './track1.jpeg',
+			coverImageUrl: '/img/track1.jpeg',
 			artistName: 'Eminem',
 			title: 'Rap God',
-			fileUrl: './Eminem_-_Rap_God.mp3',
+			fileUrl: '/source/Eminem_-_Rap_God.mp3',
 			isHit: false,
+
 		},
 		{
-			coverImageUrl: './track2.jpeg',
+			coverImageUrl: '/img/track2.jpeg',
 			artistName: '50 cent',
 			title: 'In da Club',
-			fileUrl: './50 Cent_-_In_Da_Clubs.mp3',
+			fileUrl: '/source/50 Cent_-_In_Da_Clubs.mp3',
 			isHit: true,
 		},
 	],
 }
 
-// ------------RANDER------------------------------------
+// ------------RENDER------------------------------------
 renderPlayList(playlist)
 
 
@@ -36,35 +37,51 @@ function renderPlayList(playlistForRendering) {
 	renderTrack(playlistForRendering.tracks[1]);
 }
 
+// находим элемент с классом player__track-list
+
 // ------Логотип отрисовки шапки входного плейлиста------
 function renderPlayListHeader(playlistForRendering){
-	let playListImageElement = document.createElement('img');
-	playListImageElement.src = playlistForRendering.coverImage;
-	document.body.append(playListImageElement);
+	const playerHeader = document.querySelector('.player__header');
 
+	let playListImageElement = document.createElement('img');
+	playListImageElement.classList.add('player__header-image')
+	playListImageElement.src = playlistForRendering.coverImage;
+	playerHeader.append(playListImageElement)
+	// document.body.append(playListImageElement);
+	
 	let playlistTitleElement = document.createElement('h2')
 	playlistTitleElement.append(playlistForRendering.title);
-	document.body.append(playlistTitleElement);
+	playerHeader.append(playlistTitleElement);
+
+	// document.body.append(playlistTitleElement);
 };
 
 // -----логика отрисовки входного трека------
 
 function renderTrack(inputTrackForRendering){
-	
-	let trekElement = document.createElement("div");
-	trekElement.append(inputTrackForRendering.artistName + '-' + inputTrackForRendering.title);
-	
+	// находим тег в который будет добавлен созданный плейлист
+	const playerTrackList = document.querySelector('.player__track-list');
+// создание div с треком и добавляем к нему класс
+	let trekElement = document.createElement("div"); 
+	trekElement.classList.add("track-item");
+
+	// создание тега Img для картинки трека и добавление еги в треклист
 	let coverElement = document.createElement('img');
 	coverElement.src = inputTrackForRendering.coverImageUrl;
 	trekElement.append(coverElement);
 
+	trekElement.append(inputTrackForRendering.artistName + '-' + inputTrackForRendering.title);
+	
 	let playerElement = document.createElement('audio');
+	playerElement.classList.add("track-item__custom-audio");
+
 	playerElement.src = inputTrackForRendering.fileUrl;
 	playerElement.controls = true;
 	trekElement.append(playerElement);
 
 
-	document.body.append(trekElement);
+	playerTrackList.append(trekElement);
 };
 
 
+	
